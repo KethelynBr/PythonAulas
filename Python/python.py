9e1e42400908ee00cf08f658f69fad4221c8c5ed
KethelynBr <127955594+KethelynBr@users.noreply.github.com>
2023-05-19 11:50:08 -0300

    Primeira aula da Python

---
+++ Python/python.py
@@ -0,0 +1,23 @@
+print("Oi, tudo bem?")
+
+print('Mensagem 2')
+
+print('Diego é meu "amigo"')
+
+print("Eu sou tão\"linda\"")
+
+print("1 x 1 = ",1*1)
+
+print("10² = ",10**2)
+
+print("10³ = ",10**3)
+
+print("10³ = ", pow(10,3))
+
+nome=str("")
+nome= 1
+nome=1+1
+print(nome)
+nome = True
+
+
